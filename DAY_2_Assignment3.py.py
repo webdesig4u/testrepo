64ec015ecd48d360bf166e4e33981524caf1094c
kalyanishingavi <66673964+webdesig4u@users.noreply.github.com>
2021-05-23 09:55:43 +0530

    Add files via upload
    file added from your pc

---
+++ DAY_2_Assignment3.py.py
@@ -0,0 +1,32 @@
+#assignmenrt No 3:
+#Name :-SHINGAVI KALYANI SANJAY
+#Class :-(TE_IT)
+a=int(input("Enter the Number of candidates : "))
+
+for x in range(a):
+	print("For ",x+1," candidate. ")
+	name=input("Give candidate name : ")
+	que1=input("do you know python : ")
+	que2=int(input("How good are you at maths(rate on scale of 1 to 10 : "))
+	que3=int(input("How good are you at communication : "))
+	que4=input("Are you a team player : ")
+	que5=input("Do you have any experience : ")
+
+'''Enter the Number of candidates : 2
+For  1  candidate.
+Give candidate name : Yash
+do you know python : yes
+How good are you at maths(rate on scale of 1 to 10 : 8
+How good are you at communication : 7
+Are you a team player : yes
+Do you have any experience : yes
+For  2  candidate.
+Give candidate name : Dhiraj
+do you know python : no
+How good are you at maths(rate on scale of 1 to 10 : 10
+How good are you at communication : 7
+Are you a team player : yes
+Do you have any experience : no
+
+[Program finished]
+'''
